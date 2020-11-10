b5b1ded28ccc79ee3984f6b429f7e5db1e58e63c
sean.sands <sean.sands@bellevuecollege.edu>
2020-11-09 19:25:54 -0800

    functionality get

--- app/src/main/java/com/example/loggerdemo/LoggerModel.java
+++ app/src/main/java/com/example/loggerdemo/LoggerModel.java
@@ -1,28 +1,90 @@
 package com.example.loggerdemo;
 
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorDirectChannel;
 import android.hardware.SensorManager;
+import android.net.Uri;
 import android.os.MemoryFile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class LoggerModel {
     private SensorManager mSensorManager;
+    private Sensor mGyro;
     private MemoryFile mem;
+    private SensorDirectChannel mSDC;
+    private File file;
 
-    public LoggerModel(SensorManager sm) {
+    public LoggerModel(SensorManager sm, File f) throws IOException {
         mSensorManager = sm;
-        
-        sm.createDirectChannel(mem);
+        file = f;
+        mGyro = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        mem = new MemoryFile(null, 1048575);
+
+        mSDC = sm.createDirectChannel(mem);
+
     }
 
-    public void setLogging(boolean b){
+    public void setLogging(boolean b) throws IOException {
         if(b)
         {
+            mSDC.configure(mGyro,SensorDirectChannel.RATE_NORMAL);
             System.out.println("on");
+            //return null;
         }
         else
         {
+            mSDC.configure(mGyro,SensorDirectChannel.RATE_STOP);
+            InputStream is = mem.getInputStream();
+            //Path path;
+            FileOutputStream fos = new FileOutputStream(file);
+
+            byte[] buf = new byte[8192];
+            int length;
+            while ((length = is.read(buf)) > 0) {
+                fos.write(buf, 0, length);
+            }
+
+
+
+
+            //Files.copy(is,path);
+
+            //File exportFile = new File("/storage/emulated/0/Download/keyspy/loggerdemo.logger");
+            //Intent i = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+
+            /*
+            try {
+                //Path path = FileSystems.getDefault().getPath("keyspy", "logger", "foo.bar");
+
+                //Files.createDirectories(path.getParent());
+                //Files.createFile(path);
+                //new File(path.toString());
+
+                //Files.copy(is, path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+             */
             System.out.println("off");
+            //return i;
         }
+    }
+
+    public void close(){
+        mSDC.close();
     }
 }
--- app/src/main/java/com/example/loggerdemo/MainActivity.java
+++ app/src/main/java/com/example/loggerdemo/MainActivity.java
@@ -2,29 +2,57 @@ package com.example.loggerdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
+import java.io.File;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     private LoggerModel logMod;
     private ToggleButton tbLogMode;
+    //File f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        logMod = new LoggerModel((SensorManager) getSystemService(SENSOR_SERVICE));
+        File f = new File(getExternalFilesDir(null), "loggerdemo.txt");
+        try {
+            logMod = new LoggerModel((SensorManager) getSystemService(SENSOR_SERVICE), f);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         tbLogMode = (ToggleButton)findViewById(R.id.tbLogMode);
         tbLogMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
              @Override
              public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                 logMod.setLogging(isChecked);
+                 //Intent i =
+
+                 try {
+                     logMod.setLogging(isChecked);
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+                 /*
+                 if(i!=null){
+                     startActivity(i);
+                 }
+                 */
              }
          }
 
         );
+    }
+
+    @Override
+    protected void onDestroy() {
+        logMod.close();
+        super.onDestroy();
     }
 }
